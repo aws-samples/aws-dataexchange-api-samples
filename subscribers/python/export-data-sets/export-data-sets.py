a34ea29846dc825b2154b95e13dd8e579e8e3994
Kanchan Waikar <kwwaikar@amazon.com>
2021-08-20 13:06:49 -0500

    cosmetic changes and error handling

--- subscribers/python/export-data-sets/export-data-sets.py
+++ subscribers/python/export-data-sets/export-data-sets.py
@@ -11,10 +11,10 @@ import json
 dx = boto3.client('dataexchange')
 s3 = boto3.client('s3')
 
-#This function returns revision_ids corresponding to the dataset-id specified.
-def get_revision_ids(data_set_id):
+#This function returns revision_ids corresponding to the data-set-id specified.
+def get_revisions(data_set_id):
     
-    #Paginate and extract all revisions corresponding to the dataset specified.
+    #Paginate and extract all revisions corresponding to the data-set specified.
     revisions = []
     res = dx.list_data_set_revisions(DataSetId=data_set_id)
     next_token = res.get('NextToken')
@@ -25,26 +25,22 @@ def get_revision_ids(data_set_id):
         revisions += res.get('Revisions')
         next_token = res.get('NextToken')
 
-    #Extract IDs corresponding to the revision
-    revision_ids = []
-    for rev in revisions:
-        revision_ids.append(rev['Id'])
-    #print(revision_ids)
-    return revision_ids
+    return revisions
 
 
-#This function exports assets corresponding to revision-ids specified into an S3 bucket
-def export_revisions(dataset_id,revision_ids,bucket):
+#This function exports assets corresponding to revisions specified into an S3 bucket
+def export_revisions(data_set_id,revisions,bucket):
     
-    for i in range(0, len(revision_ids), 5):
+    for i in range(0, len(revisions), 5):
         job_ids=[]
+        
         #Trigger 5 concurrent export jobs at a time
-        for revision in revision_ids[i:i + 5]:
+        for revision in revisions[i:i + 5]:
             create_job_response = dx.create_job(
                     Details={
                         'ExportRevisionsToS3': {
-                            "DataSetId": dataset_id,
-                            'RevisionDestinations':[ {"RevisionId": revision, "Bucket": bucket, "KeyPattern": "${Asset.Name}" }]
+                            "DataSetId": data_set_id,
+                            'RevisionDestinations':[ {"RevisionId": revision['Id'], "Bucket": bucket, "KeyPattern": "${Asset.Name}" }]
                     }},Type='EXPORT_REVISIONS_TO_S3'
             )
 
@@ -52,11 +48,9 @@ def export_revisions(dataset_id,revision_ids,bucket):
             job_ids.append(job_id)
             
             #Initiate the job
-            print("=> Starting Job: ",job_id, "for revision: ",revision)
+            print("=> Starting Job: ",job_id, "for revision: ",revision['Id'])
             dx.start_job(JobId=job_id)
 
-
-            
         #Wait for all import jobs to finish
         for job in job_ids:
             max_time = time.time() + 60*60 # 1 hour
@@ -65,23 +59,27 @@ def export_revisions(dataset_id,revision_ids,bucket):
                 response = dx.get_job(JobId=job_id);
                 status = response['State']
                 print('STATUS: ',job,'get_job_status'+": {}".format(status))
-                if status == "COMPLETED" or status == "ERROR":
+                if status == "COMPLETED":
                     break
+                elif status == "ERROR":
+                    print(response)
+                    print("Export failed")
+                    exit()
                 time.sleep(5)
         time.sleep(15)
 
 
-# This function accepts dataset-ids and region and exports the data into specified S3 bucket. The region of the S3 bucket and dataset must be same.
+# This function accepts data_set-ids and region and exports the data into specified S3 bucket. The region of the S3 bucket and data_set must be same.
 @click.command()
 @click.option('--bucket', '-s')
-@click.option('--dataset-ids', '-s')
+@click.option('--data-set-ids', '-s')
 @click.option('--region', '-s')
-def main(bucket,dataset_ids,region):
+def main(bucket,data_set_ids,region):
     global dx,s3
     if not bucket:
         print("No s3 bucket provided")
-    elif not dataset_ids:
-        print("No dataset_ids provided")
+    elif not data_set_ids:
+        print("No data_set_ids provided")
     if not region:
         print("No region provided")
     else:
@@ -90,11 +88,11 @@ def main(bucket,dataset_ids,region):
         dx = boto3.client('dataexchange', region_name=region)
         s3 = boto3.client('s3',region_name=region)
         
-        #loop through datasetids and extract
-        for dataset_id in dataset_ids.split(","):
-            print("Extracting revisions for Data set ### {} ###".format(dataset_id))
-            revision_ids = get_revision_ids(dataset_id)
-            export_revisions(dataset_id,revision_ids,bucket)
+        #loop through data_set_ids and extract
+        for data_set_id in data_set_ids.split(","):
+            print("Extracting revisions for Data set ### {} ###".format(data_set_id))
+            revisions = get_revisions(data_set_id)
+            export_revisions(data_set_id,revisions,bucket)
     print("Export complete.")
           
 if __name__ == '__main__':
